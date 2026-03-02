c0f3d1b46e414295a479cbab42b57b1d5794684f
alexpaynter <alexcpaynter@gmail.com>
2026-03-02 10:40:39 -0800

    Swapped the redcap splitting script to be mostly a function - moving aand cleanup next but wanted to save the comparison

--- analysis/script/split_redcap_into_tables.R
+++ analysis/script/split_redcap_into_tables.R
@@ -13,62 +13,80 @@ dat_ex <- readr::read_csv(
   col_types = cols(.default = col_character())
 )
 
-# the primary keys.
-vars_in_all_data <- c(
-  'record_id',
-  'redcap_repeat_instrument',
-  'redcap_repeat_instance'
+redcap_splitter <- function(
+  redcap_data,
+  dict,
+  keys_in_all_instr = NULL,
+  forms_missing_in_redcap = NULL
+) {
+  keys_in_all_instr <- keys_in_all_instr %||%
+    c(
+      'record_id',
+      'redcap_repeat_instrument',
+      'redcap_repeat_instance'
+    )
+
+  forms_missing_in_redcap <- forms_missing_in_redcap %||%
+    c(
+      'curation_initiation_eligibility',
+      'patient_characteristics',
+      'curation_completion',
+      'quality_assurance'
+    )
+
+  dict <- dict |>
+    mutate(
+      form_in_extract = case_when(
+        form %in% forms_missing_in_redcap ~ NA_character_,
+        T ~ form
+      )
+    )
+
+  nested_dd <- dict %>%
+    tidyr::nest(.by = 'form_in_extract', .key = 'dat_dict_sub')
+
+  nested_dd <- nested_dd |>
+    mutate(
+      var_list = purrr::map(
+        dat_dict_sub,
+        .f = \(x) x$field_name
+      )
+    )
+
+  nested_dd %<>%
+    mutate(
+      tab = purrr::map2(
+        .x = form_in_extract,
+        .y = var_list,
+        .f = \(f, v) {
+          dat_ex %>%
+            select(
+              all_of(keys_in_all_instr),
+              any_of(v)
+            ) %>%
+            filter(redcap_repeat_instrument %in% f)
+        }
+      )
+    )
+
+  nested_dd %<>%
+    mutate(
+      form_in_extract = case_when(
+        # This eventually becomes the "patient level" file.
+        is.na(form_in_extract) ~ "patient",
+        T ~ form_in_extract
+      )
+    )
+
+  nested_dd
+}
+
+
+nested_dd_new <- redcap_splitter(redcap_data = dat_ex, dict = dat_dict)
+
+nested_dd_old <- readr::read_rds(
+  '/Users/apaynter/main/projects/genie/bayer_qc/MSK-2026-02-25/data/l1_split/nested_l1.rds'
 )
-
-# Some of the "form" entries in the dictionary don't actually come in that way in the data.  Specifically these "forms" show up in the NA form in the data.
-# Originally I checked this by looking where the data was non-missing for the first field or two in the the data dictionary "form" groups.
-dat_dict %<>%
-  mutate(
-    form_in_extract = case_when(
-      form %in% 'curation_initiation_eligibility' ~ NA_character_,
-      form %in% 'patient_characteristics' ~ NA_character_,
-      form %in% 'curation_completion' ~ NA_character_,
-      form %in% 'quality_assurance' ~ NA_character_,
-      T ~ form
-    )
-  )
-
-
-nested_dd <- dat_dict %>%
-  tidyr::nest(.by = 'form_in_extract', .key = 'dat_dict_sub')
-
-nested_dd %<>%
-  mutate(
-    var_list = purrr::map(
-      dat_dict_sub,
-      .f = \(x) x$field_name
-    )
-  )
-
-nested_dd %<>%
-  mutate(
-    tab = purrr::map2(
-      .x = form_in_extract,
-      .y = var_list,
-      .f = \(f, v) {
-        dat_ex %>%
-          select(
-            all_of(vars_in_all_data),
-            any_of(v)
-          ) %>%
-          filter(redcap_repeat_instrument %in% f)
-      }
-    )
-  )
-
-nested_dd %<>%
-  mutate(
-    form_in_extract = case_when(
-      # This eventually becomes the "patient level" file.
-      is.na(form_in_extract) ~ "patient",
-      T ~ form_in_extract
-    )
-  )
 
 out_dir_l1 <- path(qc_config$storage_root, 'data', 'l1_split')
 fs::dir_create(out_dir_l1)
