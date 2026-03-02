e629b38060244fc6a17bdc9e2f063fdf168fcb60
alexpaynter <alexcpaynter@gmail.com>
2026-03-02 10:45:00 -0800

    Bug with data referenced

--- analysis/script/split_redcap_into_tables.R
+++ analysis/script/split_redcap_into_tables.R
@@ -59,7 +59,7 @@ redcap_splitter <- function(
         .x = form_in_extract,
         .y = var_list,
         .f = \(f, v) {
-          dat_ex %>%
+          redcap_data %>%
             select(
               all_of(keys_in_all_instr),
               any_of(v)
@@ -82,11 +82,13 @@ redcap_splitter <- function(
 }
 
 
-nested_dd_new <- redcap_splitter(redcap_data = dat_ex, dict = dat_dict)
+nested_dd <- redcap_splitter(redcap_data = dat_ex, dict = dat_dict)
 
 nested_dd_old <- readr::read_rds(
   '/Users/apaynter/main/projects/genie/bayer_qc/MSK-2026-02-25/data/l1_split/nested_l1.rds'
 )
+
+waldo::compare(nested_dd, nested_dd_old)
 
 out_dir_l1 <- path(qc_config$storage_root, 'data', 'l1_split')
 fs::dir_create(out_dir_l1)
