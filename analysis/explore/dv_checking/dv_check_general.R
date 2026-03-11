d927a088e08624c78d72490edad458d324aad71c
alexpaynter <alexcpaynter@gmail.com>
2026-03-11 01:30:42 -0700

    Needed the opening prayer in that script

--- analysis/explore/dv_checking/dv_check_general.R
+++ analysis/explore/dv_checking/dv_check_general.R
@@ -1,4 +1,9 @@
 # Load the new derived tables:
+library(geniedv)
+library(fs)
+library(purrr)
+purrr::walk(.x = fs::dir_ls(fs::path("R")), .f = source)
+
 multi_tab_new <- readr::read_rds(
   here('data', 'dv', 'layer_2_derived_tables', 'multisite_tables.rds')
 )
