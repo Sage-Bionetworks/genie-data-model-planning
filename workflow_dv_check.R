29136de14171932d2ff4e98b3b2ef44b1786e5e9
alexpaynter <alexcpaynter@gmail.com>
2026-03-18 11:46:43 -0700

    Added toworkflow

--- workflow_dv_check.R
+++ workflow_dv_check.R
@@ -15,5 +15,6 @@ script_runner <- function(
 script_runner('align_dd_dv_check.R')
 script_runner('split_all_redcaps.R')
 script_runner('derive_all_tables.R')
+script_runner('convenience_restrict_patients.R')
 
 # from here I've been working on explore/dv_check_general.R for now.
