db12085512e33ce56ef5f28da3b9e185ceec3d2d
alexpaynter <alexcpaynter@gmail.com>
2026-03-18 11:31:09 -0700

    naming

--- analysis/script/dv_check/convenience_restrict_patients.R
+++ analysis/script/dv_check/convenience_restrict_patients.R
@@ -3,9 +3,9 @@ library(fs)
 library(purrr)
 purrr::walk(.x = fs::dir_ls(fs::path("R")), .f = source)
 
-multisite_tables <- readr::read_rds(here(path(
+tables_new <- readr::read_rds(here(path(
   'data',
   'dv',
   'layer_2_derived_tables',
-  'multisite_tables.rds'
+  'table_list.rds'
 )))
