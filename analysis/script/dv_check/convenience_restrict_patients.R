748604e61ede565a4e831610adb688cdfba37579
alexpaynter <alexcpaynter@gmail.com>
2026-03-18 11:38:23 -0700

    added legacy table reader

--- analysis/script/dv_check/convenience_restrict_patients.R
+++ analysis/script/dv_check/convenience_restrict_patients.R
@@ -9,3 +9,39 @@ tables_new <- readr::read_rds(here(path(
   'layer_2_derived_tables',
   'table_list.rds'
 )))
+
+dir_nsclc_3.1 <- here(
+  'data-raw',
+  'bpc',
+  'step4-release',
+  'NSCLC',
+  '3.1-consortium'
+)
+
+legacy_reader <- function(file) {
+  readr::read_csv(
+    here(dir_nsclc_3.1, file)
+  ) %>%
+    filter(phase %in% "Phase II")
+}
+
+tables_legacy <- list(
+  pt = legacy_reader('patient_level_dataset.csv'),
+  ca_ind = legacy_reader('cancer_level_dataset_index.csv'),
+  ca_non_ind = legacy_reader('cancer_level_dataset_non_index.csv'),
+  reg = legacy_reader('regimen_cancer_level_dataset.csv'),
+  rad = legacy_reader('ca_radtx_dataset.csv'),
+  path = legacy_reader('pathology_report_level_dataset.csv'),
+  img = legacy_reader('imaging_level_dataset.csv'),
+  med_onc = legacy_reader('med_onc_note_level_dataset.csv'),
+  cpt = legacy_reader('cancer_panel_test_level_dataset.csv')
+)
+
+
+ca_ind <- legacy_reader('patient_level_dataset.csv')
+
+
+tables_legacy <- readr::read_csv(
+  here(dir_nsclc_3.1, 'patient_level_dataset.csv')
+) %>%
+  filter(phase %in% "Phase II")
