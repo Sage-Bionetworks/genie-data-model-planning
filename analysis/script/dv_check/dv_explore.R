48960f7e76121be52c4d3042ceda11c58c502852
alexpaynter <alexcpaynter@gmail.com>
2026-04-19 22:50:58 -0700

    Exploration file created to work through the pieces of the dmets vars

---
+++ analysis/script/dv_check/dv_explore.R
@@ -0,0 +1,23 @@
+# Load the new derived tables:
+library(geniedv)
+library(fs)
+library(purrr)
+purrr::walk(.x = fs::dir_ls(fs::path("R")), .f = source)
+
+library(testthat)
+
+restricted_tab_dir <- here('data', 'dv', 'layer_2_derived_tables')
+tables_leg <- readr::read_rds(here(
+  restricted_tab_dir,
+  'comparable_tables_legacy.rds'
+))
+tables_new <- readr::read_rds(here(
+  restricted_tab_dir,
+  'comparable_tables_new.rds'
+))
+
+derive_scan_dmets_long(
+  tables_new$img
+  tables_new$ca_ind
+)
+  
